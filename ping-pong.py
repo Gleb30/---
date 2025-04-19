da12cf06a9eb956bc65373a834b0b68ae57398dd
Глеб <pavlovgleb510@gmail.com>
2025-04-19 16:08:40 +0300

    Update ping-pong.py

--- ping-pong.py
+++ ping-pong.py
@@ -1,5 +1,14 @@
 from pygame import *
-
+from random import randint
+class GameSprite(sprite.Sprite):
+    def __init__(self,player_image, player_x, player_y, width,height, player_speed):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (width,height))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+    
 
 
 
