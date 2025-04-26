067e24f72ba62a4023aaf1e0cbe57289b508bd11
Глеб <pavlovgleb510@gmail.com>
2025-04-26 16:36:57 +0300

    Update ping-pong.py

--- ping-pong.py
+++ ping-pong.py
@@ -33,9 +33,9 @@ class Player(GameSprite):
         if keys[K_s] and self.rect.y < win_width - 80:
             self.rect.y += self.speed
 
-racket1 = Player('palka.png', 30,200,50,150,4)
-racket2 = Player('palka.png',520, 200,50,150,4)
-ball = GameSprite('tenis.png', 200,200,50,50,4)
+racket1 = Player('pal.png', 30,200,50,150,4)
+racket2 = Player('pal.png',520, 200,50,150,4)
+ball = GameSprite('shar.png', 200,200,50,50,4)
 
 font.init()
 font1 = font.Font(None,35)
@@ -60,19 +60,40 @@ while game:
         racket1.update_l()
         racket2.update_r()
 
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+
+        if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
+            speed_x *= -1
+            speed_y *= 1
+
+        if ball.rect.y > win_height-50 or ball.rect.y < 0:
+            speed_y *= -1
+
+        if ball.rect.x < 0:
+            finish = True
+            window.blit(lose1, (200,200))
+            game_over = True
+
+
+        if ball.rect.x > win_width:
+            finish = True
+            window.blit(lose2,(200,200))
+            game_over = True
+
+
         racket1.reset()
         racket2.reset()
-        ball.reest()
+        ball.reset()
     display.update()
     clock.tick(FPS)
 
 '''
 display.set_caption('пинг-понг')
 background = transform.scale(
-    image.load('фон пинг-понга.jpg'),
+    image.load('fon.jpg'),
     (win_width,win_height)
     )   
 '''
-
 
 
